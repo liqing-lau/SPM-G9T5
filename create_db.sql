4dfdf782e014a3671e0ee2b46cacedfac4fa2fa2
LILJJJJJJJ <qwxgeerbi@gmail.com>
2022-10-08 18:50:29 +0800

    update for additonal data

--- create_db.sql
+++ create_db.sql
@@ -3,6 +3,7 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 --
+drop database if exists `LJMS`;
 CREATE DATABASE IF NOT EXISTS `LJMS` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `LJMS`;
 
@@ -42,5 +43,36 @@ CREATE TABLE IF NOT EXISTS `registration` (
   `Completion_Status` varchar(20) NOT NULL,
   constraint registration_fk1 foreign key(Course_ID) references course(Course_ID),
   constraint registration_fk2 foreign key(Staff_ID) references staff(Staff_ID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `jobrole`;
+CREATE TABLE IF NOT EXISTS `jobrole` (
+  `JRole_ID` int not null primary key,
+  `JRole_Name` varchar(50) NOT NULL,
+  `JRole_Desc` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `skill`;
+CREATE TABLE IF NOT EXISTS `skill` (
+  `Skill_ID` int not null primary key,
+  `Skill_Name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `courseskill`;
+CREATE TABLE IF NOT EXISTS `courseskill` (
+  `Course_ID` varchar(20) NOT NULL,
+  `Skill_ID` int NOT NULL,
+  constraint courseskill_fk1 foreign key(Course_ID) references course(Course_ID),
+  constraint courseskill_fk2 foreign key(Skill_ID) references skill(Skill_ID),
+  constraint courseskill_pk primary key(Course_ID, Skill_ID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `jobskill`;
+CREATE TABLE IF NOT EXISTS `jobskill` (
+  `JRole_ID` int not null,
+  `Skill_ID` int NOT NULL,
+  constraint jobskill_fk1 foreign key(JRole_ID) references jobrole(JRole_ID),
+  constraint jobskill_fk2 foreign key(Skill_ID) references skill(Skill_ID),
+  constraint jobskill_pk primary key(JRole_ID, Skill_ID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- import.py
+++ import.py
@@ -31,6 +31,10 @@ role_r = pd.read_csv(r'RawData\role.csv', engine='python', encoding='unicode_esc
 course_r = pd.read_csv(r'RawData\courses.csv', engine='python', encoding='unicode_escape')
 registration_r = pd.read_csv(r'RawData\registration.csv', engine='python', encoding='unicode_escape')
 staff_r = pd.read_csv(r'RawData\staff.csv', engine='python', encoding='unicode_escape')
+jobrole_r = pd.read_csv(r'RawData\jobRole.csv', engine='python', encoding='unicode_escape')
+skill_r = pd.read_csv(r'RawData\skills.csv', engine='python', encoding='unicode_escape')
+courseskill_r = pd.read_csv(r'RawData\courseskills.csv', engine='python', encoding='unicode_escape')
+jobskill_r = pd.read_csv(r'RawData\jobskills.csv', engine='python', encoding='unicode_escape')
 
 engine = get_connection()
 conn = engine.connect()
@@ -47,3 +51,10 @@ role_r.to_sql(con = engine, name = 'role', if_exists = 'append', chunksize = 100
 course_r.to_sql(con = engine, name = 'course', if_exists = 'append', chunksize = 1000, index = False, method = insert_on_duplicate)
 staff_r.to_sql(con = engine, name = 'staff', if_exists = 'append', chunksize = 1000, index = False, method = insert_on_duplicate)
 registration_r.to_sql(con = engine, name = 'registration', if_exists = 'append', chunksize = 1000, index = False, method = insert_on_duplicate)
+jobrole_r.to_sql(con = engine, name = 'jobrole', if_exists = 'append', chunksize = 1000, index = False, method = insert_on_duplicate)
+skill_r.to_sql(con = engine, name = 'skill', if_exists = 'append', chunksize = 1000, index = False, method = insert_on_duplicate)
+courseskill_r.to_sql(con = engine, name = 'courseskill', if_exists = 'append', chunksize = 1000, index = False, method = insert_on_duplicate)
+jobskill_r.to_sql(con = engine, name = 'jobskill', if_exists = 'append', chunksize = 1000, index = False, method = insert_on_duplicate)
+
+conn.close()
+engine.dispose()
