afa3ad1ef17b138d93694891b3ff02c1dc880a47
Melissa <melissa.lee.2020@scis.smu.edu.sg>
2022-10-13 15:02:20 +0800

    Added folders

---
+++ DAO/jobRoleDAO.php
@@ -0,0 +1,63 @@
+<?php
+class jobRoleDAO{
+
+    public function getAll(){
+
+        $connMgr= new ConnectionManager();
+        $conn =$connMgr->connect();
+
+        $sql='SELECT * FROM `jobrole`';
+        $stmt = $conn->prepare($sql);
+
+        $listJR=[];
+
+        if ($stmt->execute()){
+
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+            while ($row=$stmt->fetch()){
+                $listJR[]=new jobRole(
+                                        $row["JRole_ID"],
+                                        $row["JRole_Name"],
+                                        $row["JRole_Desc"]
+                );
+            }
+
+        }
+
+        $stmt=null;
+        $conn=null;
+
+        return $listJR;
+    }
+
+    public function getRelSkills($JRole_ID){
+
+        $connMgr= new ConnectionManager();
+        $conn =$connMgr->connect();
+
+        $sql="SELECT `Skill_Name` FROM `skill` WHERE `Skill_ID` IN (SELECT `Skill_ID` FROM `jobskill` WHERE `JRole_ID`=$JRole_ID)";
+        $stmt = $conn->prepare($sql);
+
+        $listSkills=[];
+
+        if ($stmt->execute()){
+
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+            while ($row=$stmt->fetch()){
+                array_push($listSkills,$row["Skill_Name"]);
+            }
+
+        }
+
+        $stmt=null;
+        $conn=null;
+
+        return $listSkills;
+    }
+}
+
+
+
+?>
