bc850dae54edff450dc9dbdbf622000dd34b68fe
Melissa <melissa.lee.2020@scis.smu.edu.sg>
2022-10-14 16:44:33 +0800

    Changed the buttons to forms

--- Admin/HR_jobRoleView.php
+++ Admin/HR_jobRoleView.php
@@ -32,8 +32,18 @@
             }
             array_push($allJobRoles,"</ul></td>");
         }
-        array_push($allJobRoles,"<td><input type='button' value='Update' id='updateRole'>");
-        array_push($allJobRoles,"<input type='button' value='Delete' id='DeleteRole'>");
+        array_push($allJobRoles,"<td><form style='float:left; margin-block-end:0em' action='HR_jobRoleUpdate.php' method='POST'>
+                                    <input type='hidden' name='JRole_ID' value='$eachItem[0]'>
+                                    <input type='hidden' name='JRole_Name' value='$eachItem[1]'>
+                                    <input type='hidden' name='JRole_Desc' value='$eachItem[2]'>
+                                    <button name='Update' type='submit'>Update</button>
+                                    </form>
+                                    <form style='float:right; margin-block-end:0em' action='HR_jobRoleUpdate.php' method='POST'>
+                                    <input type='hidden' name='JRole_ID' value='$eachItem[0]'>
+                                    <input type='hidden' name='JRole_Name' value='$eachItem[1]'>
+                                    <input type='hidden' name='JRole_Desc' value='$eachItem[2]'>
+                                    <button name='Delete' type='submit' value='$eachItem[0]'>Delete</button>
+                                    </form>");
         array_push($allJobRoles,"</td></tr>");
     }
     $allJobRoles=implode("",$allJobRoles);
