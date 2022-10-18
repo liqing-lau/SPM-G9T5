cd0215e43728c2b79c073966f69e28796e48e2f4
Lau Li Qing <63573601+liqing-lau@users.noreply.github.com>
2022-10-18 23:37:27 +0800

    updated with toggle switch

--- Admin/authenticateAdmin.php
+++ Admin/authenticateAdmin.php
@@ -6,6 +6,8 @@
 
     $adminDAO = new adminDAO();
     $admin = $adminDAO->isAdmin($empId);
+    
+    $toggleUser = isset($_POST['false']);
 
     $userDAO = new userDAO();
     $user = $userDAO->getUser($empId);
@@ -14,9 +16,18 @@
 
     if (!$admin) {
         echo "You are not authorised to see this page, $userFirstName ($userRole)";
+    } else if (!$toggleUser) {
+        echo "You are not authorised to see this page, $userFirstName ($userRole, User)";
+        echo "<form method='post'>
+                <input type='hidden' name='empId' value=$empId>
+                <input type='submit' name='false' value='Change to Admin'></input>
+                </form>";
     } else {
-        
-        echo "Welcome $userFirstName ($userRole)";
+        echo "Welcome $userFirstName ($userRole, Admin)";
+        echo "<form method='post'>
+                <input type='hidden' name='empId' value=$empId>
+                <input type='submit' name='true' value='Change to User'></input>
+                </form>";
     }
     
 ?>
