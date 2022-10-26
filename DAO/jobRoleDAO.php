7121bfc5294b58df1fb26775860b4e778d102050
Lau Li Qing <63573601+liqing-lau@users.noreply.github.com>
2022-10-26 21:04:45 +0800

    Revert "Feature/is212 g9 t5 u2 test"

--- DAO/jobRoleDAO.php
+++ DAO/jobRoleDAO.php
@@ -80,7 +80,113 @@ class jobRoleDAO{
 
         return $namelist;
     }
-}
+
+    public function updateJobRole($JRole_ID,$JRole_Name,$JRole_Desc){
+        $connMgr= new ConnectionManager();
+        $conn = $connMgr->connect();
+
+        $sql ="UPDATE `jobrole` SET `JRole_Name`= ?, `JRole_Desc`= ? WHERE `JRole_ID`= ? ";
+
+        $stmt =$conn->prepare($sql);
+
+        $stmt->bindParam(1, $JRole_Name, PDO::PARAM_STR);
+        $stmt->bindParam(2, $JRole_Desc, PDO::PARAM_STR);
+        $stmt->bindParam(3, $JRole_ID,PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        $stmt= null;
+        $conn= null;
+        return "<br>Name and Description updated";
+    }
+
+    public function deleteRelSkills($JRole_ID,$Skill_ID){
+        $connMgr=new ConnectionManager();
+        $conn =$connMgr->connect();
+
+        $sql="DELETE FROM `jobskill` WHERE `JRole_ID`=? AND `Skill_ID`=?";
+
+        $stmt=$conn->prepare($sql);
+
+        $stmt->bindParam(1,$JRole_ID,PDO::PARAM_INT);
+        $stmt->bindParam(2,$Skill_ID,PDO::PARAM_INT);
+
+        $stmt->execute();
+        $stmt=null;
+        $conn=null;
+        return "<br>Deleted skill";
+    }
+
+    public function addRelSkills($JRole_ID,$Skill_ID){
+        $connMgr=new ConnectionManager();
+        $conn=$connMgr->connect();
+
+        $sql="INSERT INTO `jobskill` (`JRole_ID`, `Skill_ID`) VALUES (:JRole_ID, :Skill_ID)";
+
+        $stmt=$conn->prepare($sql);
+        $stmt->bindparam(':JRole_ID',$JRole_ID,PDO::PARAM_INT);
+        $stmt->bindparam(':Skill_ID',$Skill_ID,PDO::PARAM_INT);
+
+        $stmt->execute();
+        $stmt=null;
+        $conn=null;
+        return "<br>Added skill";
+    }
+
+    public function createJobRole($name, $desc, $skills) {
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->connect();
+
+        $sqlJob = "insert into jobrole
+        (JRole_Name, JRole_Desc)
+        values
+        (:name, :desc)";
+
+        $stmt = $conn->prepare($sqlJob);
+        $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+        $stmt->bindParam(':desc', $desc, PDO::PARAM_STR);
+        
+        $status = $stmt->execute();
+
+        $stmt = null;
+        $conn = null;
+
+        $jobRoleDAO = new jobRoleDAO();
+
+        $jobId = $jobRoleDAO->getIdByName($name);
+        
+        foreach ($skills as $skillId) {
+            $jobSkill = $jobRoleDAO->addJobSkill($jobId, $skillId);
+            if ($jobSkill === 0) {
+                return "An error occurred during inserting skills";
+            }
+        }
+        return $status;
+    }
+
+    public function getIdByName($name) {
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->connect();
+
+        $sql = "SELECT `JRole_ID` FROM `jobrole`
+        WHERE `JRole_Name` = :name";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+
+        while( $row = $stmt->fetch() ) {
+            $jobId = $row['JRole_ID'];
+        }
+
+        $stmt = null;
+        $conn = null;
+
+        return $jobId;
+    }
 
     public function addJobSkill($jobId, $skillId) {
         $connMgr = new ConnectionManager();
@@ -132,59 +238,6 @@ class jobRoleDAO{
 
         return true;
     }
-}
-    public function getIndividualCourseSkill($JRole_ID){
-        $connMgr = new ConnectionManager();
-        $conn = $connMgr->connect();
-
-        $sql = "SELECT C.Course_ID, C.Skill_ID, S.Skill_Name FROM courseskill C
-        LEFT JOIN skill s ON s.Skill_ID = C.Skill_ID
-         WHERE C.Skill_ID IN (SELECT S.Skill_ID FROM jobskill J, skill S WHERE J.Skill_ID = S.Skill_ID AND J.JRole_ID = $JRole_ID)
-        ";
-        $namelist = [];
-
-
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-        while( $row = $stmt->fetch() ) {
-            array_push($namelist,$row);
-        }
-        $stmt = null;
-        $conn = null;
-
-        return $namelist;
-
-    }
-
-    public function getCourseTaken($Staff_ID){
-        $connMgr = new ConnectionManager();
-        $conn = $connMgr->connect();
-
-        $sql = "SELECT r.Course_ID, Reg_Status, Completion_Status FROM registration r WHERE  Staff_ID='$Staff_ID';";
-        $takenList = [];
-
-
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-        while( $row = $stmt->fetch() ) {
-            array_push($takenList,$row);
-        }
-        $stmt = null;
-        $conn = null;
-
-        return $takenList;
-
-    }
-
-  
-    
-
-
-
 }
 
 ?>
