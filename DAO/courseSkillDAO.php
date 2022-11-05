e0c496e962ca4c71bcea41ba1b3736d3615bec49
LILJJJJJJJ <qwxgeerbi@gmail.com>
2022-11-05 19:23:30 +0800

    merged jinglin sprint3

--- DAO/courseSkillDAO.php
+++ DAO/courseSkillDAO.php
@@ -1,103 +1,68 @@
 <?php
 
-    require_once 'common.php';
+require_once 'common.php';
 
-    class courseSkillDAO {
-        public function getCourseIdBySkill($skillId) {
-            $sql = "SELECT `Course_ID` FROM `courseskill` WHERE `Skill_ID` =:skillId";
-    
-            $connMgr = new ConnectionManager();
-            $conn = $connMgr->connect();
-            
-            $courseIdList = []; 
+class CourseSkillDAO {
 
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':skillId', $skillId, PDO::PARAM_INT);
-            
-            $stmt->execute();
-    
-            $stmt->setFetchMode(PDO::FETCH_ASSOC);
-            
-            $status = false;
-    
-            while($row = $stmt->fetch()) {
-                $data = $row['Course_ID'];
-                array_push($courseIdList, $data);
-            }
-    
-            $stmt = null;
-            $conn = null;
-    
-            return $courseIdList;
-        }
-
-        public function addSkillToCourse($courseId, $skillId) {
-
-            $connMgr = new ConnectionManager();
-            $conn = $connMgr->connect();
-    
-            $sql = "INSERT INTO `courseskill`
-                    (`Course_ID`, `Skill_ID`) 
-                        VALUES
-                        (:courseId, :skillId)";
-    
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':courseId', $courseId, PDO::PARAM_STR);
-            $stmt->bindParam(':skillId', $skillId, PDO::PARAM_INT);
-    
-            $status = $stmt->execute();
-    
-            $stmt = null;
-            $conn = null;
-    
-            return $status;
-        }
-
-        public function getSkillIdList($courseId) {
-
-            $connMgr = new ConnectionManager();
-            $conn = $connMgr->connect();
-    
-            $sql = "SELECT `Skill_ID` FROM `courseskill` WHERE `Course_ID` = :courseId";
-            $list = [];
-    
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':courseId', $courseId, PDO::PARAM_STR);
-    
-            $stmt->execute();
-            $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    
-            while ($row = $stmt->fetch()) {
-                $row_Skill_ID = $row['Skill_ID'];
-                array_push($list, $row_Skill_ID);
-            }
-            $stmt = null;
-            $conn = null;
-    
-            return $list;
-        }
-
-        public function removeCourse($courseId, $Skill_ID)
-    {
+    public function checkCourseSkill($course_id,$skill_id) {
         $connMgr = new ConnectionManager();
         $conn = $connMgr->connect();
 
-        $sql = "DELETE FROM `courseskill` WHERE `Course_ID` =:courseId AND `Skill_ID` =:skillId";
+        $sql = "SELECT * FROM `courseskill`
+        WHERE `Course_ID` = :course_id and `Skill_ID` = :skill_id";
 
         $stmt = $conn->prepare($sql);
 
-        // Your code goes here
-        $stmt->bindParam(":courseId", $courseId, PDO::PARAM_STR);
-        $stmt->bindParam(":skillId", $Skill_ID, PDO::PARAM_INT);
+        $stmt->bindParam(':course_id', $course_id, PDO::PARAM_STR);
+        $stmt->bindParam(':skill_id', $skill_id, PDO::PARAM_INT);
 
-        $Skill_Status = $stmt->execute(); // DO NOT MODIFY THIS LINE
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
 
-        // Your code goes here
+        $output=[];
+
+        while( $row = $stmt->fetch() ) {
+            $cid = $row['Course_ID'];
+            $sid = $row['Skill_ID'];
+            array_push($output,[$cid,$sid]);
+        }
+
         $stmt = null;
         $conn = null;
 
-        return $Skill_Status;
+        if(empty($output)){
+            return false;
+        }
+        else{return true;}
     }
+
+    public function getCourseBySkill($skill_id) {
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->connect();
+
+        $sql = "SELECT `Course_ID` FROM `courseskill`
+        WHERE `Skill_ID` = :skill_id";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindParam(':skill_id', $skill_id, PDO::PARAM_INT);
+
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+
+        $output=[];
+
+        while( $row = $stmt->fetch() ) {
+            $cid = $row['Course_ID'];
+            array_push($output,$cid);
+        }
+
+        $stmt = null;
+        $conn = null;
+
+        return $output;
     }
+
+}
 
 ?>
